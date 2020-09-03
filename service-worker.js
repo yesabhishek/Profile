f7dfee1f8aee1aae18dec994cdccf35981c4626e
yesabhishek <34596803+yesabhishek@users.noreply.github.com>
2020-09-03 13:56:31 +0000

    Deploying to gh-pages from  @ 30640a8bfe76d82d792138d03db897aa49a1bcf0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33817c264cc09ba037ed17794ece4428.js"
+  "/yesabhishek/precache-manifest.cb71ef9f7ab36cf4c9d9830ffdb7f049.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yesabhishek/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
